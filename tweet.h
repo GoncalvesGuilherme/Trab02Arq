600730c4834bc40d148b1548cfaee3e821f3a151
nstanzani <nstanzani04@gmail.com>
2015-05-28 10:58:54 -0300

    Comentários Doxygen

--- tweet.h
+++ tweet.h
@@ -19,13 +19,92 @@ typedef struct tweet_list {
 	struct tweet_list* nextNode;
 } TweetList;
 
-int addTweet(FILE*, TWEET*);
-int removeTweet(FILE*, int);
-int readAllTweets(FILE*);
+/**
+ * @brief Insere registros no arquivo a partir
+ * da leitura dos dados
+ *
+ * @param fd Descritor do arquivo
+ * @param tweet Registro a ser adicionado
+ *
+ * @return -1 Se falhou a inserção
+ *          1 Se inseriou com sucesso
+ */
+int addTweet(FILE* fd, TWEET* tweet);
 
-int getTweetByUser(FILE*, char*);
-TweetList* getTweetByLanguage(FILE*, char*);
-TweetList* getTweetByFavorite(FILE*, int);
-TweetList* getMatchingTweet(FILE*, char*, int);
+/**
+ * @brief Remove tweet a partir do campo FAVORITE_COUNT
+ *
+ * @param fd Descritor do arquivo
+ * @param favorite Campo utilizado para buscar
+ * o tweet a ser removido
+ *
+ * @return -1 Se falhou a remoção
+ *          1 Se removido com sucesso
+ */
+int removeTweet(FILE* fd, int favorite);
+
+/**
+ * @brief Recupera todos os tweets do arquivo
+ * e imprime na tela
+ *
+ * @param fd Descritor de arquivo
+ *
+ * @return -1 Se falha na recuperação ou impressão
+ *          1 Se recuperado e impresso com sucesso
+ */
+int readAllTweets(FILE* fd);
+
+/**
+ * @brief Recupera todos os tweets referentes ao
+ * @p user e imprime na tela
+ *
+ * @param fd Descritor do arquivo
+ * @param user Username utilizado para buscar e
+ * imprimir os tweets
+ *
+ * @return -1 Se falha na recuperação ou impressão
+ *          1 Se recuperado e impresso com sucesso
+ */
+int getTweetByUser(FILE* fd, char* user);
+
+/**
+ * @brief Recupera todos os tweets referentes ao
+ * @p language
+ *
+ * @param fd Descritor do arquivo
+ * @param language Campo utilizado para buscar os
+ * tweets
+ *
+ * @return Lista com todos os tweets encontrados
+           NULL se não encontrou nenhum tweet
+ */
+TweetList* getTweetByLanguage(FILE* fd, char* language);
+
+/**
+ * @brief Recupera todos os tweets referentes ao
+ * @p favorite
+ *
+ * @param fd Descritor do arquivo
+ * @param language Campo utilizado para buscar os
+ * tweets
+ *
+ * @return Lista com todos os tweets encontrados
+           NULL se não encontrou nenhum tweet
+ */
+TweetList* getTweetByFavorite(FILE* fd, int favorite);
+
+/**
+ * @brief Recupera todos os tweets que estão em
+ * ambos os arquivos
+ *
+ * @param fd Descritor do arquivo
+ * @param language Campo utilizado para buscar os
+ * tweets
+ * @param favorite Campo utilizado para buscar os tweets
+ *
+ * @return Lista com todos os tweets que deram match
+           NULL se não encontrou nenhum tweet
+ */
+TweetList* getMatchingTweet(FILE* fd, char* language, int favorite);
 
 #endif
